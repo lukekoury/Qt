e2ebc74ef55f7d601ac6521fb46a871987b70bd8
lukekoury <36520299+lukekoury@users.noreply.github.com>
2018-02-15 16:36:58 -0500

    Add files via upload

---
+++ untitled/main.cpp
@@ -0,0 +1,22 @@
+#include <FEHLCD.h>
+#include <FEHIO.h>
+#include <FEHUtility.h>
+
+int main(void)
+{
+
+    float x,y;
+
+    LCD.Clear( FEHLCD::Black );
+    LCD.SetFontColor( FEHLCD::White );
+
+    while( true )
+    {
+        if( LCD.Touch(&x,&y) )
+        {
+            LCD.WriteLine( "Hello World!" );
+            Sleep( 100 );
+        }
+    }
+    return 0;
+}
