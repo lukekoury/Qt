afd8fb270096a522fabd459e7a54e9b09cbee788
sweeneyde <36520290+sweeneyde@users.noreply.github.com>
2018-02-15 21:29:10 -0500

    Update OmniMotion.cpp

--- OmniMotion.cpp
+++ OmniMotion.cpp
@@ -5,6 +5,14 @@
 #include <FEHRPS.h>
 using namespace std;
 
+#define PI_180 0.0174532925
+#define SPEEDCONSTANT 1.0
+
+void moveAtAngleRelRobot(float heading, float speedPercent)
+void moveAtAngleRelCourse(float heading, float speedPercent);
+void setRotation(float direction);
+void halt();
+
 FEHMotor motorFL(FEHMotor::Motor0,7.2);
 FEHMotor motorFR(FEHMotor::Motor1,7.2);
 FEHMotor motorBL(FEHMotor::Motor2,7.2);
@@ -19,28 +27,39 @@ void setWheels(float fl, float fr, float bl, float br){
   motorBR.setPercent(100.0*br);
 }
 
-void setVelocityComponents(float right, float forward, fload speedPercent){
+float setVelocityComponents(float right, float forward, float speedPercent){
   //arguments between -1 and 1
 	//Ex: setVelocityComponents(-1, 1, 0.75);
   //  will set robot going forward and to the left at 75% Speed
-  
+	//returns speed
+
   float fl=forward+right;
   float fr=-forward+right;
-  float br=-forward-right;
   float bl=forward-right;
-  
-	//Scale back so max speed and no overflow:
-  float m=max( max(abs(fr), abs(fl)), max(abs(bl), abs(br)) );
-  if(abs(m)<.001){
-    fl\=m; fr\=m; br\=m; bl\=m;
+  float br=-forward-right;
+
+	//Scale back so speed is maximum and no overflow:
+  float m=max( max( abs(fl),abs(fr) ), max( abs(bl),abs(br) ) );
+  if(abs(m)>.001){
+    fl/=m; fr/=m; br/=m; bl/=m;
   }
   setWheels(fl*speedPercent,fr*speedPercent,bl*speedPercent,br*speedPercent);
+	return speedPercent * SPEEDCONSTANT * sqrt(fl*fl+fr*fr);
 }
 
-void moveRelRobot(float heading, float speedPercent){
-	setVelocityComponents(cos heading, sin heading, 1.0);
+float moveAtAngleRelRobot(float heading, float speedPercent){
+	return setVelocityComponents(cos(PI_180 * heading), sin(PI_180 * heading), 1.0);
 }
 
-void moveRelCourse(float heading, float speedPercent){
-	setRobotHeading(heading-robotHeading, float speedPercent)
+float moveAtAngleRelCourse(float heading, float speedPercent){
+	return moveAtAngleRelRobot(heading-90/*Robot Orientation*/, speedPercent);
+}
+
+void setRotation(float direction){
+  if(abs(direction)>1) direction/=abs(direction);
+  setWheels(direction, direction, direction, direction);
+}
+
+void halt(){
+  setWheels(0,0,0,0);
 }
