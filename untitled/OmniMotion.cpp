99f44fea4fda5c7b47aedd98a3bb27a852b9d0be
Dennis Sweeney <sweeneyde@osu.edu>
2018-02-16 16:16:07 -0500

    added logging command

--- untitled/OmniMotion.cpp
+++ untitled/OmniMotion.cpp
@@ -3,6 +3,7 @@
 #include <FEHLCD.h>
 #include <FEHUtility.h>
 #include <FEHRPS.h>
+#include <FEHSD.h>
 using namespace std;
 
 #define PI_180 0.0174532925
@@ -31,13 +32,30 @@ struct {
 
 int main(){
   RPS.InitializeTouchMenu();
+  OpenLog();
+  updatePosition();
+  CloseLog();
   //example: box starting from top corner
 }
 
-void updatePosition(){
-  RobotPosition.x = X();
-  RobotPosition.y = Y();
-  RobotPosition.heading = Heading();
+bool updatePosition(){
+  float x = X();
+  float y = Y();
+  float heading = Heading();
+  if(abs(x-(-1))>.001){
+    RobotPosition.x = X();
+    RobotPosition.y = Y();
+    RobotPosition.heading = Heading();
+    doc("Position: ", x,y,heading);
+    return true;
+  }
+  doc("Position Update Failed: "x,y,heading);
+  return false;
+}
+
+void doc(const char * text, float a=nan, float b=nan, float c=nan){
+  LCD.WriteLine(text+to_string(a)+to_string(b)+to_string(c));
+  SD.Printf(to_string(TimeNow())+text+to_string(a)+to_string(b)+to_string(c));
 }
 
 void setWheels(float fl, float fr, float bl, float br){
@@ -54,7 +72,6 @@ float setVelocityComponents(float right, float forward, float speedPercent){
 	//Ex: setVelocityComponents(-1, 1, 0.75);
   //  will set robot going forward and to the left at 75% Speed
 	//returns speed
-
   float fl=forward+right;
   float fr=-forward+right;
   float bl=forward-right;
@@ -65,43 +82,65 @@ float setVelocityComponents(float right, float forward, float speedPercent){
   if(abs(m)>.001){
     fl/=m; fr/=m; br/=m; bl/=m;
   }
-  setWheels(fl*speedPercent,fr*speedPercent,bl*speedPercent,br*speedPercent);
-	return speedPercent * SPEEDCONSTANT * sqrt(fl*fl+fr*fr);
+  if(abs(speedPercent)>.001){
+    fl*=speedPercent; fr*=speedPercent; br*=speedPercent; bl*=speedPercent;
+  }
+
+  setWheels(fl,fr,bl,br);
+
+  //pythagorean between two components of motion to return speed
+  float speed = SPEEDCONSTANT * sqrt(fl*fl+fr*fr);
+  doc("Wheels set: ",fl,fr);
+  doc("Velocity:", right, forward, speed);
+	return speed;
 }
 
 float moveAtAngleRelRobot(float heading, float speedPercent){
+  doc("RelRobot: ", heading, speedPercent);
 	return setVelocityComponents(cos(PI_180 * heading), sin(PI_180*heading), 1.0);
 }
 
 float moveAtAngleRelCourse(float heading, float speedPercent){
-	return moveAtAngleRelRobot(heading-(0/*Robot Orientation*/), speedPercent);
+  doc("RelCourse: ", heading, speedPercent);
+	return moveAtAngleRelRobot(heading-RobotPosition.heading, speedPercent);
 }
 
 void setRotation(float direction){
   if(abs(direction)>1) direction/=abs(direction);
+  doc("Setting Rotation:", direction);
   setWheels(direction, direction, direction, direction);
 }
 void rotateBy(float angle){
+  doc("Rotating by:", angle)
 	setRotation(angle>0? 1:-1);
 	timeEnd=TimeNow()+angle/ROTATIONCONSTANT;
 	while(TimeNow()<timeEnd){
 		//Do Stuff while rotating
 	}
   halt();
+  doc("Rotation Finished.");
 }
 void rotateTo(float heading){
-	currentHeading=0/*ROBOT HEADING*/;
+  heading %=360;
+	currentHeading=RobotPosition.heading%360;
+  float rotationAngle=0;
 	if(abs(heading-currentHeading)<180){
-		rotateBy(heading-currentHeading);
-	}else{
-		rotateBy((heading-currenHeading)-360);
-	}
+		rotationAngle=heading-currentHeading);
+	}else if(heading>currentHeading){
+		rotationAngle=-currentHeading+(heading-360)
+	}else if(heading<currentHeading){
+    rotationAngle=(360-currentHeading)+heading
+  }
+  doc("Rot from/to/by:", currentHeading, heading, rotationAngle);
+  rotateBy(rotationAngle);
 	//Maybe check and adjust?
 }
 void moveBlind(float angle, float distance){
+  doc("BlindMove", angle, distance);
 	float speed = moveAtAngleRelCourse(angle, 1.0);
-	float endTime = TimeNow()+distance/Speed;
+	float endTime = TimeNow()+distance/speed;
 	while(timeNow()<endTime);
+  doc("BlindMove finished");
 }
 void moveTo(float x, float y){
 	float speed = moveAtAngleRelCourse(atan2(y-currentY, x-currentx), 1.0);
@@ -110,5 +149,6 @@ void moveTo(float x, float y){
 }
 
 void halt(){
+  doc("Halting");
   setWheels(0,0,0,0);
 }
