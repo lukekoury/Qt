4b366732dc13274013eb2b99f39c193c9802a886
Dennis Sweeney <sweeneyde@osu.edu>
2018-02-21 11:05:41 -0500

    pt1

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -42,10 +42,12 @@ void doc(const char *text, float a);
 void doc(const char *text, float a, float b);
 void doc(const char *text, float a, float b, float c);
 void doc(const char *text, float a, float b, float c, float d);
-bool startWithCds();
 void setupRun();
+bool startWithCds();
 void calibrateCds();
+void cdsMeterMode();
 void calibrateRPS();
+void rpsMeterMode();
 void waitForTouch();
 bool updatePosition();
 
@@ -60,6 +62,7 @@ void setRotation(float direction);
 float principal(float x);
 float arg(float x1,float y1,float x2,float y2);
 float pythag(float x1,float y1,float x2,float y2);
+float deltaAngle(float from, float to);
 
 void moveBlind(float angle, float distance, float speedPercent);
 void moveBlindTo(float x, float y, float speedPercent);
@@ -72,15 +75,22 @@ void rotateTo(float heading);
 // OVERALL PROCEDURE ##########################################################
 
 int main(){
-    //RPS.InitializeTouchMenu();
+    RPS.InitializeTouchMenu();
     setupRun();
     LCD.WriteRC("Touch to domniate.",6,4);
     waitForTouch();
     startWithCds();
+    //cdsMeterMode();
 
     //
 
-    moveTo1(-12,-12);
+    //Performance Test 1
+    moveTo1(-12,-22);
+    moveBlindTo(-7,-22,1.00);
+    moveTo1(-12,-1);
+    moveTo1(-12,18,1.00);
+    moveTo1(12,18,1.00);
+    moveTo1(12,-18,1.00);
 
     //
 
@@ -98,14 +108,14 @@ void setupRun(){
 
 void calibrateCds(){
     //takes 1.2 seconds
-    LCD.WriteRC("Touch to calubrate CdS.",6,4);
+    LCD.WriteLine("Touch to calubrate CdS.");
     waitForTouch();
 
     float sum=0;
-    int numCalibrations=20;
+    int numCalibrations=50;
     for(int i=0; i<numCalibrations; i++){
         sum+=cds.Value();
-        Sleep(10);
+        Sleep(4);
     }
     cdsControl = sum / numCalibrations;
     doc("cds control:", cdsControl);
@@ -113,10 +123,10 @@ void calibrateCds(){
 void cdsMeterMode(){
     while(true){
         float sum=0;
-        int numCalibrations=20;
+        int numCalibrations=50;
         for(int i=0; i<numCalibrations; i++){
             sum+=cds.Value();
-            Sleep(10);
+            Sleep(4);
         }
         cdsControl = sum / numCalibrations;
         LCD.Clear();
@@ -128,6 +138,19 @@ void calibrateRPS(){
     startY=RPS.Y();
     updatePosition();
 }
+void rpsMeterMode(){
+    while(true){
+        LCD.Clear();
+        LCD.Write("( ");
+        LCD.Write(RPS.X());
+        LCD.Write(" , ");
+        LCD.Write(RPS.Y());
+        LCD.Write(" )\n");
+        LCD.WriteLine(RPS.Heading());
+        Sleep(100);
+    }
+}
+
 void waitForTouch(){
     float x,y;
     while(LCD.Touch(&x,&y)) Sleep(1); //until untouched
@@ -136,24 +159,26 @@ void waitForTouch(){
     Buzzer.Beep();
 }
 bool startWithCds(){
-    float readings[20]={4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4};
-    float sum=4*20;
+    int numreadings = 50;
+    float readings[numreadings];
+    for(int i=0;i<numreadings;i++)readings[i]=4;
+    float sum=4*numreadings;
     float m=cdsControl*0.8;//threshhold in volts TODO:Calibrate
     int panicTime = TimeNow() + 60; //after 60 seconds, go anyway.
-    while(sum/20>m){
+    while(sum/numreadings>m){
         if(TimeNow()>panicTime){
             doc("Going without CdS.");
             return false;
         }
         //move only after average of 20 under threshold.
         sum-=readings[0];
-        for(int i=0;i<19;i++)
+        for(int i=0;i<numreadings-1;i++)
             readings[i]=readings[i+1];
-        readings[19]=cds.Value();
-        sum+=readings[19];
-        Sleep(5);
+        readings[numreadings-1]=cds.Value();
+        sum+=readings[numreadings-1];
+        Sleep(2);
     }
-    doc("Going with CdS.", sum/20, m);
+    doc("Going with CdS.", sum/numreadings, m);
     return true;
 }
 
@@ -196,11 +221,11 @@ void doc(const char *text, float a, float b, float c, float d){
 // MOVEMENT AND NAVIGATION ###################################################
 
 bool updatePosition(){
-    //THis is the only place we need to worry about how RPS works.
+    //This is the only place we need to worry about how RPS works.
     float x = RPS.X()-startX;
     float y = RPS.Y()-startY;
     float heading = RPS.Heading();
-    if(fabs(heading)>0){
+    if(fabs(heading)>0||fabs(x)>0||fabs(y)>0){
         if(Robot.x == x && Robot.y == y && Robot.heading == heading){
             //Do not update timestamp if RPS hasn't been updated
         } else {
@@ -264,17 +289,9 @@ void moveTo2(float x, float y){
 
 void rotateTo(float heading){
     updatePosition();
-    float to = principal(heading);
-    float from = principal(Robot.heading);
-    float rotationAngle=0;
-    if(fabs(to-from)<180){
-        rotationAngle=to-from;
-    }else if(to>from){
-        rotationAngle=-from+(to-360);
-    }else if(to<from){
-        rotationAngle=(360-from)+to;
-    }
-    doc("Rot from/to/by:", from, to, rotationAngle);
+    float rotationAngle=deltaAngle(Robot.heading, heading);
+    doc("Rot from/to/by:", Robot.heading, heading, rotationAngle);
+
     float angleSpeed=1;
     if(fabs(rotationAngle)<90) angleSpeed=fabs(rotationAngle/90);
     rotateBy(rotationAngle, angleSpeed);
@@ -282,7 +299,7 @@ void rotateTo(float heading){
     //Check and adjust until within 5 degrees
     if(updatePosition()){
         float newAngle = principal(heading-Robot.heading);
-        if(newAngle>5 && newAngle<355){
+        if( fabs(deltaAngle(Robot.heading, heading))>5 ){
             rotateTo(heading);
         }
     } else {
@@ -306,8 +323,8 @@ void moveBlind(float angle, float distance, float speedPercent){
 }
 
 void moveBlindTo(float x, float y, float speedPercent){
-    float angle = (1/PI_180)*atan2(y-Robot.y, x-Robot.x);
-    float distance = sqrt(pow(y-Robot.y,2)+pow(x-Robot.x,2));
+    float angle = arg(Robot.x, Robot.y, x, y);
+    float distance = pythag(Robot.x, Robot.y, x, y);
     moveBlind(angle, distance, speedPercent);
 }
 
@@ -393,4 +410,17 @@ float arg(float x1, float y1, float x2, float y2){
 float pythag(float x1, float y1, float x2, float y2){
     //returns the distance from (x1,y1) to (x2,y2)
     return sqrt(pow(y2-y1,2)+pow(x2-x1,2));
+}
+float deltaAngle(float from, float to){
+    to = principal(to);
+    from = principal(from);
+    float rotationAngle=0;
+    if(fabs(to-from)<180){
+        rotationAngle=to-from;
+    }else if(to>from){
+        rotationAngle=-from+(to-360);
+    }else if(to<from){
+        rotationAngle=(360-from)+to;
+    }
+    return rotationAngle;
 }
